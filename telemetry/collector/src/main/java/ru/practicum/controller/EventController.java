1c78871000444bf5261e88ad801658040063915e
f1zik <tkyskii2004@gmail.com>
2025-09-29 00:37:44 +0500

    Проверка #3

--- telemetry/collector/src/main/java/ru/practicum/controller/EventController.java
+++ telemetry/collector/src/main/java/ru/practicum/controller/EventController.java
@@ -1,36 +1,60 @@
 package ru.practicum.controller;
 
-import jakarta.validation.Valid;
+import com.google.protobuf.Empty;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ru.practicum.event.hub.base.HubEvent;
-import ru.practicum.service.event.EventService;
-import ru.practicum.event.sensor.base.SensorEvent;
+import net.devh.boot.grpc.server.service.GrpcService;
+import ru.practicum.mapper.ProtoMapper;
+import ru.practicum.service.sensor.SensorService;
+import ru.yandex.practicum.grpc.telemetry.collector.CollectorControllerGrpc;
+import ru.yandex.practicum.grpc.telemetry.collector.CollectorControllerOuterClass.Ack;
+import ru.yandex.practicum.grpc.telemetry.event.HubEventProto;
+import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
 
-@RestController
+@GrpcService
 @Slf4j
-@RequestMapping("/events")
 @RequiredArgsConstructor
-public class EventController {
+public class EventController extends CollectorControllerGrpc.CollectorControllerImplBase {
 
-    private final EventService eventService;
+    private final ProtoMapper protoMapper;
+    private final SensorService sensorService;
 
-    @PostMapping(path = "/sensors", consumes = "application/json")
-    public ResponseEntity<Void> collectSensor(@Valid @RequestBody SensorEvent event) {
-        log.debug("Incoming /events/sensors: {}", event);
-        eventService.publishSensorEvent(event);
-        return ResponseEntity.accepted().build();
+    @Override
+    public void collectSensorEvent(SensorEventProto proto, StreamObserver<Ack> response) {
+        try {
+            sensorService.handle(protoMapper.toDomain(proto));
+            response.onNext(ackOk("Sensor event received"));
+            response.onCompleted();
+        } catch (Exception e) {
+            response.onError(asInternal(e));
+        }
     }
 
-    @PostMapping(path = "/hubs", consumes = "application/json")
-    public ResponseEntity<Void> collectHub(@Valid @RequestBody HubEvent event) {
-        log.debug("Incoming /events/hubs: {}", event);
-        eventService.publishHubEvent(event);
-        return ResponseEntity.accepted().build();
+    @Override
+    public void collectHubEvent(HubEventProto proto, StreamObserver<Ack> response) {
+        try {
+            log.debug("Hub event: hubId={}, payload={}", proto.getHubId(), proto.getPayloadCase());
+            response.onNext(ackOk("hub received"));
+            response.onCompleted();
+        } catch (Exception e) {
+            response.onError(asInternal(e));
+        }
+    }
+
+    @Override
+    public void ping(Empty request, StreamObserver<Ack> response) {
+        response.onNext(ackOk("pong"));
+        response.onCompleted();
+    }
+
+    private static Ack ackOk(String msg) {
+        return Ack.newBuilder().setStatus("OK").setMessage(msg).build();
+    }
+
+    private static StatusRuntimeException asInternal(Exception e) {
+        return new StatusRuntimeException(Status.INTERNAL.withDescription(e.getMessage()).withCause(e));
     }
 }
