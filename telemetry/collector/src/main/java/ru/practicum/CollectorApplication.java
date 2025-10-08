7deebca662a82acb2a8afb6b5827025c5aa18db6
f1zik <tkyskii2004@gmail.com>
2025-10-09 02:07:26 +0500

    Collector #5

--- telemetry/collector/src/main/java/ru/practicum/CollectorApplication.java
+++ telemetry/collector/src/main/java/ru/practicum/CollectorApplication.java
@@ -1,20 +1,15 @@
 package ru.practicum;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import ru.practicum.kafka.KafkaConfigProperties;
-import ru.practicum.kafka.KafkaEventProducer;
 
 @SpringBootApplication
 @EnableConfigurationProperties({KafkaConfigProperties.class})
 @Slf4j
 public class CollectorApplication {
-
-    @Autowired
-    private KafkaEventProducer kafkaEventProducer;
 
     public static void main(String[] args) {
         SpringApplication.run(CollectorApplication.class, args);
