2e58e1bc25adfee6edecafdac6636c5a92220c64
f1zik <tkyskii2004@gmail.com>
2025-10-08 20:09:55 +0500

    Проверка временно

--- telemetry/collector/src/main/java/ru/practicum/handler/sensor/type/LightSensorEventHandler.java
+++ telemetry/collector/src/main/java/ru/practicum/handler/sensor/type/LightSensorEventHandler.java
@@ -1,40 +1,40 @@
-package ru.practicum.handler.sensor.type;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-import ru.practicum.handler.sensor.SensorEventHandler;
-import ru.practicum.kafka.KafkaEventProducer;
-import ru.practicum.mapper.ProtoMapper;
-import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
-import java.time.Instant;
-
-@Slf4j
-@Component
-@RequiredArgsConstructor
-public class LightSensorEventHandler implements SensorEventHandler {
-
-    private final KafkaEventProducer kafkaProducer;
-    private final ProtoMapper protoMapper;
-
-    @Override
-    public SensorEventProto.PayloadCase getMessageType() {
-        return SensorEventProto.PayloadCase.LIGHT_SENSOR_EVENT;
-    }
-
-    @Override
-    public void handle(SensorEventProto event) {
-        var light = event.getLightSensorEvent();
-
-        log.info("Обработка события датчика освещенности: hubId={}, luminosity={}, linkQuality={}",
-                event.getHubId(), light.getLuminosity(), light.getLinkQuality());
-
-        var avroEvent = protoMapper.toAvro(event);
-        String sensorEventsTopic = "telemetry.sensors.v1";
-
-        kafkaProducer.send(sensorEventsTopic, event.getHubId(), Instant.now(), avroEvent);
-
-        log.debug("Событие датчика освещенности отправлено в Kafka: hubId={}, topic={}",
-                event.getHubId(), sensorEventsTopic);
-    }
-}
+//package ru.practicum.handler.sensor.type;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Component;
+//import ru.practicum.handler.sensor.SensorEventHandler;
+//import ru.practicum.kafka.KafkaEventProducer;
+//import ru.practicum.mapper.ProtoMapper;
+//import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
+//import java.time.Instant;
+//
+//@Slf4j
+//@Component
+//@RequiredArgsConstructor
+//public class LightSensorEventHandler implements SensorEventHandler {
+//
+//    private final KafkaEventProducer kafkaProducer;
+//    private final ProtoMapper protoMapper;
+//
+//    @Override
+//    public SensorEventProto.PayloadCase getMessageType() {
+//        return SensorEventProto.PayloadCase.LIGHT_SENSOR_EVENT;
+//    }
+//
+//    @Override
+//    public void handle(SensorEventProto event) {
+//        var light = event.getLightSensorEvent();
+//
+//        log.info("Обработка события датчика освещенности: hubId={}, luminosity={}, linkQuality={}",
+//                event.getHubId(), light.getLuminosity(), light.getLinkQuality());
+//
+//        var avroEvent = protoMapper.toAvro(event);
+//        String sensorEventsTopic = "telemetry.sensors.v1";
+//
+//        kafkaProducer.send(sensorEventsTopic, event.getHubId(), Instant.now(), avroEvent);
+//
+//        log.debug("Событие датчика освещенности отправлено в Kafka: hubId={}, topic={}",
+//                event.getHubId(), sensorEventsTopic);
+//    }
+//}
