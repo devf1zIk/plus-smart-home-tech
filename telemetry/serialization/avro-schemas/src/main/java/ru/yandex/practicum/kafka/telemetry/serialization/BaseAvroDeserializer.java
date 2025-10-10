123bc8d281443d5e0fcebc05bab095eae708b766
f1zik <tkyskii2004@gmail.com>
2025-10-10 17:46:06 +0500

    Analyzer #14

--- telemetry/serialization/avro-schemas/src/main/java/ru/yandex/practicum/kafka/telemetry/serialization/BaseAvroDeserializer.java
+++ telemetry/serialization/avro-schemas/src/main/java/ru/yandex/practicum/kafka/telemetry/serialization/BaseAvroDeserializer.java
@@ -1,18 +1,17 @@
 package ru.yandex.practicum.kafka.telemetry.serialization;
 
 import org.apache.avro.Schema;
-import org.apache.avro.io.BinaryDecoder;
 import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Deserializer;
-import java.util.Map;
+import java.io.ByteArrayInputStream;
 
 public class BaseAvroDeserializer<T extends SpecificRecordBase> implements Deserializer<T> {
-
+    private final Schema schema;
     private final DecoderFactory decoderFactory;
-    private final DatumReader<T> reader;
 
     public BaseAvroDeserializer(Schema schema) {
         this(DecoderFactory.get(), schema);
@@ -20,25 +19,19 @@ public class BaseAvroDeserializer<T extends SpecificRecordBase> implements Deser
 
     public BaseAvroDeserializer(DecoderFactory decoderFactory, Schema schema) {
         this.decoderFactory = decoderFactory;
-        this.reader = new SpecificDatumReader<>(schema);
+        this.schema = schema;
     }
-
-    @Override
-    public void configure(Map<String, ?> configs, boolean isKey) {}
 
     @Override
     public T deserialize(String topic, byte[] data) {
-        if (data == null) return null;
-        try {
-            BinaryDecoder decoder = decoderFactory.binaryDecoder(data, null);
-            return reader.read(null, decoder);
-        } catch (Exception e) {
-            throw new DeserializationException("Ошибка десереализации данных из топика [" + topic + "]", e);
+        if (data == null) {
+            return null;
         }
-    }
-
-    @Override
-    public void close() {
-        Deserializer.super.close();
+        try {
+            DatumReader<T> reader = new SpecificDatumReader<>(schema);
+            return reader.read(null, decoderFactory.binaryDecoder(new ByteArrayInputStream(data), null));
+        } catch (Exception e) {
+            throw new SerializationException("Ошибка десериализации данных", e);
+        }
     }
 }
